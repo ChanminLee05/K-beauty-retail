24be1f4a39846457584ff6a12f558bd37223a22a
Chanmin Lee <lee00837@algonquinlive.com>
2024-11-27 00:47:45 -0700

    login, logout, register using firebase

---
+++ src/Components/Features/SignIn.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { auth } from '../../Config/config'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { ToastContainer, toast } from 'react-toastify';
+import "./Sign.css";
+
+export default function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    
+    async function handleSignIn(e) {
+        try {
+            e.preventDefault();
+            setEmail('');
+            setPassword('');
+
+            await signInWithEmailAndPassword(auth, email, password);
+            toast.success("Logged in Successfully!", { position: "top-center", hideProgressBar: true });
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 1500);
+        } catch (error) {
+            toast.error(error.message, { position: "bottom-center" });
+        }
+    }
+  return (
+    <div className='sign-page'>
+        <div className="sign-container">
+            <h1>Login</h1>
+            <form className='form-container' autoComplete='off' onSubmit={handleSignIn}>
+                <label htmlFor='email'>Email</label>
+                <input type='text' className='form-control' id='email' placeholder='Email'required onChange={(e) => setEmail(e.target.value)} value={email}></input>
+                <label htmlFor='password'>Password</label>
+                <input type='text' className='form-control' id='password' placeholder='Password' required onChange={(e) => setPassword(e.target.value)} value={password}></input>
+                <button className="btn btn-primary sign-btn" type='submit'>Login</button>
+                <span>New User?<a href='/sign-up'>Register</a></span>
+            </form>
+        </div>
+        <ToastContainer />
+    </div>
+  )
+}
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react'
+import "./Navbar.css"
+import { auth, db } from '../../Config/config'
+import { getDoc, doc } from 'firebase/firestore';
+import { ToastContainer, toast } from 'react-toastify';
+import Logo from "../../Assets/clown.png";
+
+export default function Navbar() {
+  const [userName, setUserName] = useState(null);
+
+  async function fetchUserName() {
+    auth.onAuthStateChanged(async (user) => {
+      // console.log(user)
+      if (user) {
+        const docRef = doc(db, "Users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setUserName(docSnap.data())
+        } else {
+          console.log("User is not logged in")
+        }
+      } else {
+        setUserName(null);
+      }
+    })
+  }
+
+  async function handleLogOut() {
+    try {
+      await auth.signOut();
+      toast.success("Logged out successfully!", { position: "top-center", hideProgressBar: true });
+            setTimeout(() => {
+                window.location.href = "/";
+            }, 1500);
+    } catch(error) {
+      toast.error(error.message, { position: "bottom-center" });
+    }
+  }
+
+  useEffect(() => {
+    fetchUserName();
+    return () => fetchUserName();
+  },[])
+
+  useEffect(() => {
+    function handleResize() {
+      const dropdown = document.querySelector('.nav-item.drop');
+      if (dropdown) {
+        if (window.innerWidth < 992) {
+          dropdown.classList.add('dropend');
+          dropdown.classList.remove('dropdown');
+        } else {
+          dropdown.classList.add('dropdown');
+          dropdown.classList.remove('dropend');
+        }
+      }
+    }
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+            <img src={Logo} alt='logo-img' className='logo-img' /><span>BrandNew</span>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              {userName ? (
+                <span className="user-section">
+                  <span className="greeting">Hello,</span>
+                  {userName.firstName}
+                </span>
+              ) : (<a href="/login">Login</a>)}
+            </li>
+            <li className="nav-item drop">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Products
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/toner">Toner</a></li>
+                <li><a className="dropdown-item" href="/lotion">Lotion</a></li>
+                <li><a className="dropdown-item" href="/serum">Serum</a></li>
+              </ul>
+            </li>
+            {userName ? (
+              <li className="nav-item">
+              <button className="nav-link" onClick={handleLogOut}>Logout</button>
+            </li>
+            ) : <></>}
+            <li className="nav-item">
+              <a className="nav-link position-relative" href="/cart">
+                  <i class="fa-solid fa-cart-shopping"></i>
+                  <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                    <span class="visually-hidden">cart items</span>
+                  </span>
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+      <ToastContainer />
+</nav>
+  )
+}
