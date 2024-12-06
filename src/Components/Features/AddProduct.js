0cc931ca9015efe2f274fda8e80002537698ce52
Chanmin Lee <lee00837@algonquinlive.com>
2024-12-06 08:02:15 -0700

    add product, fetch data, filter

--- src/Components/Features/AddProduct.js
+++ src/Components/Features/AddProduct.js
@@ -1,81 +1,166 @@
 import React, { useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
-import { db, storage } from "../../Config/config";
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { saveProductData } from "../../Config/config";
+import { useHandleImage } from './HandleImage';
+import "./AddProduct.css";
 
 export default function AddProduct() {
-    const [pName, setPName] = useState('');
-    const [pDesc, setPDesc] = useState('');
-    const [pPrice, setPPrice] = useState('');
-    const [image, setImage] = useState(null);
+    const [brand, setBrand] = useState('');
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [price, setPrice] = useState('');
+    const [productType, setProductType] = useState('');
+    const [size, setSize] = useState('');
+    const [rating, setRating] = useState('');
+    const [skinType, setSkinType] = useState('');
+    const [ingredients, setIngredients] = useState('');
 
-    const types = ['image/jpg', 'image/jpeg', 'image/png', 'image/PNG'];
-    function handleAddProduct(e) {
+    const { selectedImageUrl, setSelectedImageUrl, fileInputRef, handleImageChange } = useHandleImage();
+    
+    async function handleSave(e) {
         e.preventDefault();
-        // console.log(pName, pDesc, pPrice)
-        // console.log(image)
-        if (!image) {
-            toast.error("Please select an image!", { position: "top-center" });
+
+        if (!selectedImageUrl) {
+            toast.error("Please upload a product image.", { position: "top-center" });
             return;
         }
 
-        const storageRef = ref(storage, `product-images/${image.name}`);
-        const uploadTask = uploadBytesResumable(storageRef, image);
-
-        uploadTask.on('state_changed', snapShot => {
-            const progress = (snapShot.bytesTransferred/snapShot.totalBytes) * 100;
-            console.log(progress)
-        }, (error) => {
-                toast.error(error.message, { position: "top-center" });
-            }, () => {
-                getDownloadURL(storageRef)
-            .then(url => {
-                db.collection('Products').add({
-                    pName,
-                    pDesc,
-                    pPrice: Number(pPrice),
-                    url
-                }).then(() => {
-                    toast.success("Product added successfully!", { position: "top-center", hideProgressBar: true });
-                    setPName('');
-                    setPDesc('');
-                    setPPrice('');
-                    document.getElementById('file').value = '';
-                }).catch(error => toast.error(error.message, { position: "top-center" }));
-            }).catch((error) => {
-                toast.error(error.message, { position: "top-center" });
-            });
-        })
-    }
-
-    function handleProductImg(e) {
-        let selectedFile = e.target.files[0];
-        if (selectedFile) {
-            if (selectedFile && types.includes(selectedFile.type)) {
-                setImage(selectedFile)
-            } else {
-                setImage(null);
-                toast.error("Please select a valid image file type (png or jpg)", { position: "bottom-center" });
-            }
-        } else {
-            setImage(null);
-            toast.error("Please select your file", { position: "bottom-center" });
+        try {
+            await saveProductData(brand, title, desc, price, productType, size, rating, skinType, ingredients, selectedImageUrl);
+            toast.success("Product saved successfully!", { position: "top-center", hideProgressBar: true });
+            setBrand('')
+            setTitle('')
+            setDesc('')
+            setPrice('')
+            setProductType('')
+            setSize('')
+            setRating('')
+            setSkinType('')
+            setIngredients('')
+            document.getElementById('file').value = '';
+            setSelectedImageUrl(null);
+        } catch (error) {
+            toast.error("Error saving product. Please try again.", { position: "top-center", hideProgressBar: true });
         }
     }
+
   return (
-    <div className='sign-page'>
-        <div className="sign-container">
+    <div className='add-product-page'>
+        <div className="add-product-container">
             <h1>Add Products</h1>
-            <form className='form-container' autoComplete='off' onSubmit={handleAddProduct}>
-                <label htmlFor='pName'>Product Title</label>
-                <input type='text' className='form-control' id='pName' required onChange={(e) => setPName(e.target.value)} value={pName}></input>
-                <label htmlFor='pDesc'>Product Description</label>
-                <input type='text' className='form-control' id='pDesc' required onChange={(e) => setPDesc(e.target.value)} value={pDesc}></input>
-                <label htmlFor='pPrice'>Product Price</label>
-                <input type='text' className='form-control' id='pPrice' required onChange={(e) => setPPrice(e.target.value)} value={pPrice}></input>
-                <label htmlFor='file'>Upload Product Image</label>
-                <input type='file' className='form-control' id='file' onChange={handleProductImg}/>
-                <button className="btn btn-primary sign-btn" type='submit'>SUBMIT</button>
+            <form className='add-product-form-container' autoComplete='off' onSubmit={handleSave}>
+                <div className="form-inside">
+                    <div className="form-left">
+                        <label htmlFor='brand'>Product Brand</label>
+                        <input
+                                type="text"
+                                id="brand"
+                                className="form-control"
+                                required
+                                value={brand}
+                                onChange={(e) => setBrand(e.target.value)}
+                            />
+                        <label htmlFor="title">Product Title</label>
+                        <input
+                            type="text"
+                            id="title"
+                            className="form-control"
+                            required
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                        <label htmlFor="desc">Product Description</label>
+                        <input
+                            type="text"
+                            id="desc"
+                            className="form-control"
+                            required
+                            value={desc}
+                            onChange={(e) => setDesc(e.target.value)}
+                        />
+                        <label htmlFor="price">Product Price</label>
+                        <input
+                            type="text"
+                            id="price"
+                            className="form-control"
+                            required
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+                        <label htmlFor="productType">Product Type</label>
+                        <select
+                            id="productType"
+                            className="form-control"
+                            required
+                            value={productType}
+                            onChange={(e) => setProductType(e.target.value)}
+                        >
+                            <option value="">Select Product Type</option>
+                            <option value="lotion">Lotion</option>
+                            <option value="toner">Toner</option>
+                            <option value="serum">Serum</option>
+                        </select>
+                        <label htmlFor="size">Product Size</label>
+                        <input
+                            type="text"
+                            id="size"
+                            className="form-control"
+                            required
+                            value={size}
+                            onChange={(e) => setSize(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-right">
+                        <label htmlFor="rating">Product Rating</label>
+                        <input
+                            type="number"
+                            id="rating"
+                            className="form-control"
+                            required
+                            min="0"
+                            max="5"
+                            step="0.1"
+                            value={rating}
+                            onChange={(e) => setRating(e.target.value)}
+                        />
+                        <label htmlFor="skinType">Skin Type</label>
+                        <select
+                            id="skinType"
+                            className="form-control"
+                            required
+                            value={skinType}
+                            onChange={(e) => setSkinType(e.target.value)}
+                        >
+                            <option value="">Select Skin Type</option>
+                            <option value="normal">Normal</option>
+                            <option value="dry">Dry</option>
+                            <option value="oily">Oily</option>
+                            <option value="combination">Combination</option>
+                            <option value="sensitive">Sensitive</option>
+                        </select>
+                        <label htmlFor="price">Ingredients</label>
+                        <textarea
+                            type="text"
+                            id="ingredients"
+                            className="form-control"
+                            required
+                            value={ingredients}
+                            autoCorrect= "on"
+                            onChange={(e) => setIngredients(e.target.value)}
+                        />
+                        <label htmlFor="file">Upload Product Image</label>
+                        <input
+                            type="file"
+                            id="file"
+                            className="form-control"
+                            ref={fileInputRef}
+                            accept="image/*"
+                            onChange={(event) => handleImageChange(event, title)} 
+                        />
+                        {selectedImageUrl && <img src={selectedImageUrl} alt="Preview" className='preview-img' style={{width: "30px", height: "30px"}}/>}
+                    </div>
+                </div>
+                <button className="btn btn-primary" type='submit'>SUBMIT</button>
             </form>
         </div>
         <ToastContainer />
--- src/Components/Products/Toner.js
+++ src/Components/Products/Toner.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Navbar from '../Navbar/Navbar'
+import SideBar from './SideBar/SideBar'
+import ProductList from './Product/ProductList'
+import { handlePriceFilter, handleSkinTypeFilter, handleRatingFilter } from '../Features/FilterFunctions';
+import { auth } from '../../Config/config'
 
 export default function Toner() {
+  const [priceFilter, setPriceFilter] = useState('');
+  const [skinTypeFilter, setSkinTypeFilter] = useState('');
+  const [ratingFilter, setRatingFilter] = useState('');
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserName(user.displayName || 'User');
+      } else {
+        setUserName(null);
+      }
+    });
+  }, []);
+
   return (
-    <div>Toner</div>
+    <>
+      <Navbar userName={userName} />
+      <div className='product-page row'>
+        <SideBar 
+          title="Toners"
+          setPriceFilter={setPriceFilter}
+          setSkinTypeFilter={setSkinTypeFilter}
+          setRatingFilter={setRatingFilter}
+          handlePriceFilter={handlePriceFilter}
+          handleSkinTypeFilter={handleSkinTypeFilter}
+          handleRatingFilter={handleRatingFilter}
+        />
+        <div className="right-section col-10">
+          <ProductList 
+            priceFilter={priceFilter}
+            skinTypeFilter={skinTypeFilter}
+            ratingFilter={ratingFilter}
+            productType="toner"
+            userName={userName}
+          />
+        </div>
+      </div>
+    </>
   )
 }
