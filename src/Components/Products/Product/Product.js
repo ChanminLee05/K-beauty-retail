67c67c05544f867f78f33dfedcdc2c355a56f665
Chanmin Lee <lee00837@algonquinlive.com>
2024-12-19 15:16:32 -0700

    added addCart feature

--- src/Components/Products/Product/Product.js
+++ src/Components/Products/Product/Product.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { addFavorite, deleteFavorite, checkIfFavorited } from '../../../Config/config';
+import { addFavorite, deleteFavorite, checkIfFavorited, addCart, deleteCart, checkIfCarted } from '../../../Config/config';
 import { auth } from '../../../Config/config';
 
 export default function Product({ brand, title, price, productType, rating, skinType, imageUrl, userName }) {
     const [favorites, setFavorites] = useState({});
+    const [carts, setCarts] = useState({});
     const [isMobile, setIsMobile] = useState(window.innerWidth < 992);
     
     async function handleFavorite(title) {
@@ -33,7 +34,34 @@ export default function Product({ brand, title, price, productType, rating, skin
       } catch (error) {
           console.error("Error handling favorites:", error);
       }
-  }
+    }
+
+    async function handleCart(title) {
+      if (!userName) {
+          alert("Please log in to use the carts feature.");
+          return;
+      }
+  
+      try {
+          const isCurrentlyCarted = carts[title];
+          setCarts((prev) => ({
+              ...prev,
+              [title]: !isCurrentlyCarted,
+          }));
+  
+          const userId = auth.currentUser?.uid;
+          if (!userId) {
+              alert("User is not authenticated.");
+              return;
+          }
+  
+          if (!isCurrentlyCarted) {
+              await addCart(userId, title);
+          }
+      } catch (error) {
+          console.error("Error handling favorites:", error);
+      }
+    }
 
     function renderStars(rating) {
       const fullStars = Math.floor(rating);
@@ -83,17 +111,21 @@ export default function Product({ brand, title, price, productType, rating, skin
 
   return (
     <div className='card product'>
-        <button className="favorite-btn" onClick={() => handleFavorite(title)}>
+        <button className="favorite-btn" type='button' onClick={() => handleFavorite(title)}>
             {favorites[title] ? <i className="fa-solid fa-heart"></i> : <i className="fa-regular fa-heart"></i>}
         </button>
         <img src={imageUrl} className="card-img-top" alt="..." onClick={goToProductDetail}/>
-        <div className="card-body" onClick={goToProductDetail}>
+        <div className="card-body">
             <p><b>{brand}</b></p>
             <h5 className="card-title">{title}</h5>
             {isMobile ? null : <p className="card-text">{renderStars(rating)}</p>}
             <p className='price'><b>${price}</b></p>
             <p style={{display: "none"}}>{productType}</p>
             <p style={{display: "none"}}>{skinType}</p>
+            <button className="cart-btn" type="button" onClick={() => {handleCart(title); }}>
+              <i className="fa-solid fa-cart-shopping cart-icon"></i>
+              Add to Cart
+            </button>
         </div>
     </div>
   )
